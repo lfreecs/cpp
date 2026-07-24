25cdce352a35027e308b64726d1a4cac3020a832
lfreecs <feqv2009@gmail.com>
2026-07-24 09:31:30 -0400

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vl = vector<ll>;
+using pll = pair<ll, ll>;
+using vpp = vector<pll>;
+#define ff first
+#define ss second
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    
+
+}
