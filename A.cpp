c310c707ddefa63323dad1909ae4af3a6f77ffe5
lfreecs <feqv2009@gmail.com>
2026-07-28 09:10:48 -0400

    Enhance A.cpp with additional type aliases
    Added new type aliases for pairs and vectors.

--- A.cpp
+++ A.cpp
@@ -3,16 +3,17 @@ using namespace std;
 typedef long long ll;
 using vl = vector<ll>;
 using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using vs = vector<string>;
+using pdi = pair<double, int>;  
 using vpp = vector<pll>;
 #define ff first
 #define ss second
 
-
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-
+    ios::sync_with_stdio(0);cin.tie(nullptr);
     
 
+    
 }
