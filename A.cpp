3c116445f91c3035fa2176da8e4d2a587c6aa0f3
lfreecs <feqv2009@gmail.com>
2026-08-01 20:56:48 -0400

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -2,18 +2,24 @@
 using namespace std;
 typedef long long ll;
 using vl = vector<ll>;
+using vi = vector<int>;
 using pll = pair<ll, ll>;
 using pii = pair<int, int>;
 using vs = vector<string>;
-using pdi = pair<double, int>;  
+using pdi = pair<double, int>;
 using vpp = vector<pll>;
 #define ff first
 #define ss second
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int lo, int hi)
+{
+    uniform_int_distribution<int> dist(lo, hi);
+    return dist(rng);
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);cin.tie(nullptr);
-    
-
-    
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
 }
